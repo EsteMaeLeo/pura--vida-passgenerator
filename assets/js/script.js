9dbc450b382ba5d218f2abd38370777e26f1086b
EsteMaeLeo <leonel_morales20@hotmail.com>
2022-01-08 15:21:10 -0600

    add lenght password and validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 // Assignment code here
-
+//special characters
+var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~", "\""];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -13,9 +14,42 @@ function writePassword() {
 
 }
 
-var generatePassword = function (){
 
-return "MyPassword!22";
+var passwordUser = {
+  lengthPass: "",
+  //validate length is between 8 and 128
+  valState: true,
+  lowcase: "",
+  getLengthPass: function () {
+    debugger;
+    this.lengthPass = "";
+    while (this.lengthPass === "" || this.lengthPass === null) {
+
+      this.lengthPass = prompt("Please enter the length of the password at least 8 characters and no more than 128 characters?")
+
+      //validate if value is not empty convert value to integer
+      if (this.lengthPass !== "" && this.lengthPass !== null) {
+        //convert the value to integer
+        this.lengthPass = parseInt(this.lengthPass);
+        if (this.lengthPass < 8 || this.lengthPass > 128) {
+          window.alert("Length of the password does not meet criteria");
+          //clear value to keep loop until input the correct lenght of the password
+          this.lengthPass = "";
+        }
+      }
+
+
+    }
+  }
+}
+
+var generatePassword = function () {
+  console.log(specialChar);
+  console.log(specialChar[13]);
+  passwordUser.getLengthPass();
+  return passwordUser.lengthPass;
+
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
