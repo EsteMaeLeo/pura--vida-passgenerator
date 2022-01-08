a5cfc35b3da4fd32304446479a6cd09783f37f52
EsteMaeLeo <leonel_morales20@hotmail.com>
2022-01-08 15:54:56 -0600

    add new prompt windows to the application

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,32 +13,53 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-
+//object password
 var passwordUser = {
   lengthPass: "",
   //validate length is between 8 and 128
   valState: true,
-  lowcase: "",
-  getLengthPass: function () {
-    debugger;
+  character: false,
+  numeric: false,
+  lowercase: false,
+  uppercase: false,
+  setLengthPass: function () {
+debugger;
     this.lengthPass = "";
     while (this.lengthPass === "" || this.lengthPass === null) {
+      valLenghtPassword(passwordUser);
 
-      this.lengthPass = prompt("Please enter the length of the password at least 8 characters and no more than 128 characters?")
+    }
+  },
 
-      //validate if value is not empty convert value to integer
-      if (this.lengthPass !== "" && this.lengthPass !== null) {
-        //convert the value to integer
-        this.lengthPass = parseInt(this.lengthPass);
-        if (this.lengthPass < 8 || this.lengthPass > 128) {
-          window.alert("Length of the password does not meet criteria");
-          //clear value to keep loop until input the correct lenght of the password
-          this.lengthPass = "";
-        }
-      }
+  setspecialChar: function () {
+    this.character = window.confirm("Click OK to confirm including special characters");
+  },
 
+  setNumeric: function(){
+    this.numeric = window.confirm("Click OK to confirm including numerics characters");
+  },
 
+  setLowercase: function(){
+    this.lowercase = window.confirm("Click OK to confirm including lowercase characters");
+  },
+
+  setUppercase: function(){
+    this.uppercase = window.confirm("Click OK to confirm including uppercase characters");
+  }
+
+}
+
+var valLenghtPassword = function (password) {
+  password.lengthPass = prompt("Please enter the length of the password at least 8 characters and no more than 128 characters?")
+
+  //validate if value is not empty convert value to integer
+  if (password.lengthPass !== "" && password.lengthPass !== null) {
+    //convert the value to integer
+    password.lengthPass = parseInt(password.lengthPass);
+    if (password.lengthPass < 8 || password.lengthPass > 128) {
+      window.alert("Length of the password does not meet criteria");
+      //clear value to keep loop until input the correct lenght of the password
+      password.lengthPass = "";
     }
   }
 }
@@ -46,7 +67,11 @@ var passwordUser = {
 var generatePassword = function () {
   console.log(specialChar);
   console.log(specialChar[13]);
-  passwordUser.getLengthPass();
+  passwordUser.setLengthPass();
+  passwordUser.setspecialChar();
+  passwordUser.setNumeric();
+  passwordUser.setLowercase();
+  passwordUser.setUppercase();
   return passwordUser.lengthPass;
 
 }
