cd94e1a1cb20cb13aa75e6287e9f1f592d34ca20
EsteMaeLeo <leonel_morales20@hotmail.com>
2022-01-09 23:34:47 -0600

    add new changes for picking random options with a function

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,9 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  //after display clean variables
+  passwordUser.passwordOption = [];
+  password2: "";
 }
 //object password
 var passwordUser = {
@@ -27,13 +29,13 @@ var passwordUser = {
     position: 0,
   },
 
-  optionLowercase:{
+  optionLowercase: {
     lowercase: false,
-    position:1,
+    position: 1,
   },
-  OptionUpperCase:{
+  OptionUpperCase: {
     uppercase: false,
-    position:0,
+    position: 0,
   },
   passwordOption: [],
   password2: "",
@@ -56,7 +58,7 @@ var passwordUser = {
       this.optionCharacter.position = 1;
       this.counterOption++;
       this.passwordOption.push(this.optionCharacter.position);
-      
+
     }
   },
 
@@ -117,34 +119,65 @@ var randomNumber = function (min, max) {
   return value;
 };
 
+//function to generate array with random pick of options for character: special, numeric, upper and lower
+//validate if there is missing option put into the array
+var createArray = function (randomArray) {
+  //create random numer of option to pick characters
+  for (var i = 0; i < passwordUser.lengthPass; i++) {
+
+    var randomOption = passwordUser.passwordOption[Math.floor(Math.random() * passwordUser.passwordOption.length)]
+    randomArray.push(randomOption);
+  }
+  //validate against array passwordOption if there is  missing in the new array
+  var missing = 0;
+  for (var i = 0; i < passwordUser.passwordOption.length; i++) {
+    if (randomArray.indexOf(passwordUser.passwordOption[i]) == -1) {
+      //missing.push(passwordUser.passwordOption[i]);
+      missing = passwordUser.passwordOption[i];
+    }
+  }
+  //if found a missing now check repeat number and replace with the missing
+  let duplicates = []
+  if(missing !== 0){
+    for (var i = 0; i < randomArray.length; i++) {
+      if (randomArray[i + 1] === randomArray[i]) {
+        randomArray[i + 1] = missing;
+      }
+    }
+  }
+
+}
 
 var createUserPass = function () {
   var counter = 1;
   var randomIndex = 0;
   passwordTemp = [];
   passwordUser.password2 = "";
+   var noFound = false;
 
-  for (var i = 0; i < passwordUser.lengthPass; i++) {
-    var optChar = randomNumber(1, passwordUser.counterOption);
-    if(counter>4){
-      counter=1;
-    }
-    counter++;
-    if(counter>4){
-      counter=1;
-    }
-  }
   console.log(passwordUser.passwordOption);
-  for (var i = 0; i < passwordUser.lengthPass; i++) {
-    randomIndex = passwordUser.passwordOption[Math.floor(Math.random() * passwordUser.passwordOption.length)]
-    passwordTemp.push(randomIndex);
-  }
+  /*for (var i = 0; i < passwordUser.lengthPass; i++) {
+    var randomOption = passwordUser.passwordOption[Math.floor(Math.random() * passwordUser.passwordOption.length)]
+    if (i === passwordUser.passwordOption.length) {
+      for (var j = 0; j < passwordUser.passwordOption; j++) {
+        for (var k = 0; k < passwordTemp.length; k++) {
+          if (passwordUser.passwordOption[j] !== passwordTemp.length[k]) {
+
+          }
+        }
+      }
+    } else {
+      passwordTemp.push(randomIndex);
+    }
+  }*/
+  createArray(passwordTemp);
   console.log(passwordTemp);
+
   for (var i = 0; i < passwordUser.lengthPass; i++) {
     //random to pick different character 
     var optChar = randomNumber(1, passwordUser.counterOption);
-  
-   // passwordUser.passwordOption.push(optChar);
+
+    // passwordUser.passwordOption.push(optChar);
 
     switch (optChar) {
       case passwordUser.optionCharacter.position:
